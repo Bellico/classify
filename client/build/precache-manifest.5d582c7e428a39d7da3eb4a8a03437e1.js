16473ad23ff150b9694400827966feb0016c4ef5
Martin Franck <franck.martin12@hotmail.com>
2020-02-02 23:54:37 +0100

    Add build file

---
+++ client/build/precache-manifest.5d582c7e428a39d7da3eb4a8a03437e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc23f48a35025107a3659712e631987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a80c9fd78f7458f189",
+    "url": "/static/css/main.74ae56b7.chunk.css"
+  },
+  {
+    "revision": "6d652984502e36516a38",
+    "url": "/static/js/2.70b6db91.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.70b6db91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6a80c9fd78f7458f189",
+    "url": "/static/js/main.3beb9cc4.chunk.js"
+  },
+  {
+    "revision": "91495336b65a5acc059b",
+    "url": "/static/js/runtime-main.3984ebf1.js"
+  },
+  {
+    "revision": "e1ec5b0657f640556edf758bd1173ad2",
+    "url": "/static/media/classify.e1ec5b06.svg"
+  }
+]);
